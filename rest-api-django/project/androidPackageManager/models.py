9ffc4f50916945aff6fc4cc2ee334a938612f857
francois.moine <francois.moine@epitech.eu>
2019-06-03 01:59:07 +0200

    create model androidPackage

--- rest-api-django/project/androidPackageManager/models.py
+++ rest-api-django/project/androidPackageManager/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class AndroidPackage(models.Model):
+   application = models.TextField()
+   package_name = models.TextField()
+   package_version_code = models.TextField()
+   created_at = models.DateTimeField(auto_now_add=True)
+
+   def __str__(self):
+      return "{0} {1}".format(self.package_name, self.package_version_code)
